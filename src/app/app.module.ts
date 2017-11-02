c0f940f3a3e73d4c3e611ecddd09e8258deec44d
Victor Carvalho <victorcarvalhosp@gmail.com>
2017-11-02 16:33:55 -0200

    adicionado e configurado firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {AngularFireModule, FirebaseAppConfig} from "angularfire2";
+
+const firebaseAppConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyADpOCvJq80A8xgO50YDLkIqxpR3UMvgbk",
+  authDomain: "corpchat-ionic2.firebaseapp.com",
+  databaseURL: "https://corpchat-ionic2.firebaseio.com",
+  storageBucket: "corpchat-ionic2.appspot.com",
+  messagingSenderId: "251201162260"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +23,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseAppConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
